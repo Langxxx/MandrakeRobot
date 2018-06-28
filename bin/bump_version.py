4769158eaea52115f03b87bd74deb99e52dd19ff
Langxxx <316230604@qq.com>
2018-06-28 15:35:38 +0800

    Improve bump version script

--- bin/bump_version.py
+++ bin/bump_version.py
@@ -5,6 +5,10 @@ from common import production
 
 @production
 def run(consumer):
+    rebase_dev_cmd = "git reset --hard && git checkout dev && git pull --rebase origin dev"
+    consumer.send(rebase_dev_cmd)
+    consumer.send(exec_cmd(rebase_dev_cmd))
+
     bump_cmd = 'fastlane increment_build_number_then_commit'
     consumer.send(bump_cmd)
     exec_cmd(bump_cmd)
